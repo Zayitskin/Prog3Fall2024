aaca0528a7f869d92cc9594b0a8fcbf8d51c7af6
Bugga314 <149637529+Bugga314@users.noreply.github.com>
2024-10-17 14:00:24 -0500

    Update client.py
    setting up the client, idk what's gonna break or not. What address do we use

--- client.py
+++ client.py
@@ -5,5 +5,18 @@ import socket
 from sock_coro import co_recv
 from sock_coro import co_send
 
+ADDR = "127.0.0.1"
+PORT = 1234
+
+def server_connect():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((ADDR, PORT))
+        send = b""
+        while send != b"close":
+            recv = sock.recv(1024)
+            print(recv.decode(encoding="UTF-8"))
+            send = bytes(input("Input: "), encoding="UTF-8")
+            sock.send(send)
+
 if __name__ == "__main__":
-    pass
+    server_connect()
