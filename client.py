ca5021f134e5551921e1e39b1c6fc2cc63c95ec6
Bugga314 <149637529+Bugga314@users.noreply.github.com>
2024-10-22 10:36:05 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,4 @@
-
-import asyncio
 import socket
-
-from sock_coro import co_recv
-from sock_coro import co_send
 
 ADDR = "127.0.0.1"
 PORT = 1234
@@ -11,6 +6,7 @@ PORT = 1234
 def server_connect():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((ADDR, PORT))
+        print(f"Server found. Connecting to {ADDR}:{PORT}")
         send = b""
         while send != b"close":
             recv = sock.recv(1024)
